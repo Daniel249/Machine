781f5f9753a6aaa27ad64b7426ae0e131a474ced
Daniel249 <danielmorillo249@hotmail.com>
2016-12-23 14:27:36 +0100

    accidental commit

--- Program.cs
+++ Program.cs
@@ -3,13 +3,6 @@
 
 class pro {
     public static void Main(string[] args) {
-        machine mach = new machine();
-        bool noch = true;
-        // just test, get loop to machine
-        while(noch) {
-            mach.determine();
-            Console.WriteLine("esta es la version");
-            //mach.memory().printReg(); //
-        }
+        Environment 
     }         
 }
